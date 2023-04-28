bb433f1bee8d4c66b0d157dd23119e3fcad03e0e
Corey Carpenter <stephcorey7@gmail.com>
2023-04-27 18:46:08 -0700

    started js that can call omdb api and get movies for search bar

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const movieSearchBox = document.getElementById("movie-search-box");
+const searchList = document.getElementById("search-list")
+
+//loads the movies from OMDb API
+async function loadMovies(_searchTerm) {
+    const URL = "https://omdbapi.com/?s=${searchTerm}&page=1&apikey=b4d0ef09";
+    //fetches results from api based on search term
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    console.log(data.Search);
+    if (data.response == "True") displayMovieList(data.Search);
+};
+
+//finds movies on keyup
+//TODO: add hide-search-list id that displays none
+//TODO: 
+function findMovies() {
+    let searchTerm = (movieSearchBox.value);
+    if (searchTerm.length > 0) {
+        loadMovies(searchTerm);
+    } else { 
+        searchList.classList.add('hide-search-list');
+    };
+};
