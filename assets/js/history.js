52f7cdc332d0bcba87d5ed6eaeaf642acce4d18e
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-05-04 16:54:04 -0700

    centered headings, added year element to youtube search API and history page

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,16 +1,21 @@
 var lastMovieName;
 var lastMoviePlot;
+var lastMovieYear;
 var displayDiv = document.querySelector("#past-result-grid")
 
 function lastMovieTitle() {
   lastMovieName =  localStorage.getItem("Title")
   lastMoviePlot = localStorage.getItem("Plot")
+  lastMovieYear = localStorage.getItem("Year")
   var title = document.createElement("h1")
   title.textContent = lastMovieName
   title.classList.add("movie-title")
+  var year = document.createElement("h3")
+  year.textContent = lastMovieYear
   var plot = document.createElement("h3")
   plot.textContent = lastMoviePlot
   displayDiv.appendChild(title)
+  displayDiv.appendChild(year)
   displayDiv.appendChild(plot)
 }
 
